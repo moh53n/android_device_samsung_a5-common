c34820861b5a3459d7a8624166fe310a343c0d2b
Mohsen Tahmasebi <moh53n@outlook.com>
2021-05-11 22:12:49 +0430

    Fix BoardConfigCommon.mk
    Add TARGET_ARCH_VARIANT
    Change sepolicy mode to permissive for now
    Change kernel toolchain to arm-eabi-7.2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,14 +36,14 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 # Arch
 TARGET_CPU_VARIANT          := cortex-a53
 TARGET_CPU_CORTEX_A53       := true
-
+TARGET_ARCH_VARIANT         := armv8-a
 # Kernel
 TARGET_KERNEL_ARCH            := arm
 TARGET_LINUX_KERNEL_VERSION   := 3.10
 BOARD_CUSTOM_BOOTIMG          := true
 BOARD_CUSTOM_BOOTIMG_MK       := hardware/samsung/mkbootimg.mk
 BOARD_DTBTOOL_ARGS            := -2
-BOARD_KERNEL_CMDLINE          := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=enforcing
+BOARD_KERNEL_CMDLINE          := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
 BOARD_KERNEL_BASE             := 0x80000000
 BOARD_RAMDISK_OFFSET          := 0x02000000
 BOARD_KERNEL_TAGS_OFFSET      := 0x01e00000
@@ -56,7 +56,7 @@ TARGET_KERNEL_SELINUX_CONFIG  := selinux_defconfig
 BOARD_KERNEL_IMAGE_NAME       := zImage
 
 # Toolchain
-KERNEL_TOOLCHAIN            := $(BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN            := $(BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.2/bin
 KERNEL_TOOLCHAIN_PREFIX     := arm-eabi-
 
 # Partition sizes and filesystems
